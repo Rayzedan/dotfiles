7f0111c393238c525d399c2859b882fa7f24806a
d.bezlepkin <d.bezlepkin@prosoftsystems.ru>
2025-04-09 14:40:40 +0500

    deleted old neovim config

--- nvim/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePre" }, {
-  callback = function()
-    vim.cmd("silent %s/\\s\\+$//ge")
-    vim.cmd("silent %s/\\t/    /ge")
-  end,
-})
